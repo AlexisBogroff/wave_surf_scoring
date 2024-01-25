e84bbace41383607827e2936f471dca41f400a8b
C-0-S-M-0-S <85568120+C-0-S-M-0-S@users.noreply.github.com>
2024-01-25 14:37:09 +0100

    colab model to script, to be tested

--- pages/Judges.py
+++ pages/Judges.py
@@ -2,6 +2,12 @@ import streamlit as st
 import youtube_dl
 import cv2
 import os
+import re
+import math
+import torch
+import pathlib
+from super_gradients.training import models
+from super_gradients.common.object_names import Models
 
 st.set_page_config(
     page_icon='🎯',
@@ -72,6 +78,50 @@ if not video_upload:
         st.text('Please upload a video or a link.')
 else:
     if st.button('Launch inference', type='primary'):
-        pass
-        ### MODEL HERE ###
+        yolo_nas_pose = models.get("yolo_nas_pose_l", pretrained_weights="coco_pose").cuda()
+        def make_prediction(input_file, action, confidence=0.55):
+            """
+            Make a prediction using the fixed model and device, and either show or save the result.
 
+            Args:
+            - input_file (str): Path to the input file.
+            - action (str): Either 'show' or 'save'.
+            - confidence (float, optional): Confidence threshold. Defaults to 0.75.
+
+            Returns:
+            - None
+
+            Raises:
+            - ValueError: If the action is not 'show' or 'save'.
+            """
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+            if action == "show":
+                yolo_nas_pose.to(device).predict(input_file, conf=confidence).show()
+                model_pred = yolo_nas_pose.predict(input_file, conf=confidence)[0].prediction.poses
+            elif action == "save":
+                output_file = pathlib.Path(input_file).stem + "-detections" + pathlib.Path(input_file).suffix
+                yolo_nas_pose.to(device).predict(input_file, conf=confidence).save(output_file)
+                print(f"Prediction saved to {output_file}")
+            else:
+                raise ValueError("Action must be either 'show' or 'save'.")
+
+            return model_pred
+
+        frames = os.listdir(f'frames')
+        frames.sort(key=lambda x: int(re.search('[0-9]+', x).group(0)))
+        for frame in frames:
+            print(f'Prediction for frame : {frame}')
+            make_prediction(f'frames/{frame}', 'show')
+
+        pred = make_prediction('frames/16800.jpg', 'show')
+        pred[0]
+
+        def euclidian(point1, point2):
+            return math.sqrt(pow(point2[0] - point1[0], 2) + pow(point2[1] - point1[1], 2))
+        point_list = [[4, 6], # Oreille gauche, épaule gauche
+              [6, 12], # épaule gauche, hanche gauche
+              [12, 14], # hanche gauche, genoux gauche
+              [14, 16]] # genoux gauche, pied gauche
+        length = sum([euclidian(pred[0][point1], pred[0][point2]) for point1, point2 in point_list])
+        length
