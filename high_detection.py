e781fe0e3775d3c64439506ca07325db389e1a08
Nathan HUGUEN <152524024+NathanHUGUEN@users.noreply.github.com>
2024-01-25 14:32:26 +0100

    Update high_detection.py
    Modification des fonctions pour plus de clarté, tests d'amélioration de l'algo de detection de haut de vague

--- high_detection.py
+++ high_detection.py
@@ -64,7 +64,7 @@ class image_vague:
                 self.surfeur_haut = y
                 self.surfeur_bas = y + height
     
-                cv2.rectangle(self.image_box, (x, y), (x + width, y + height), color=(0, 255, 0), thickness=2)
+                #cv2.rectangle(self.image_box, (x, y), (x + width, y + height), color=(0, 255, 0), thickness=2)
     
         #self.affichage_image("Image", self.image_box)
             
@@ -88,49 +88,11 @@ class image_vague:
  
         image_with_contours = np.zeros_like(self.image_original)
         cv2.drawContours(image_with_contours, contours, -1, (0, 255, 0), 2)
- 
+        cv2.imshow("Image Segmentee", self.image_segmented)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         self.image_segmented = image_with_contours
             
-    def dessiner_lignes_vertes_2(self,step=10,threshold_min=0,threshold_max=100):
- 
-        for stepX in range(step, 40, 5):
-          for threshold_high in range(threshold_min+10,threshold_max, 10):
-            for threshold_low in range(threshold_min, threshold_high, 10):
-              #reinitialisatio des variables
-              output_img = self.image_box.copy()
-              self.lines_above_surfer = 0
-              last_y_center = -1
-              self.detecter_contours(threshold_low, threshold_high)
-              gray_image = cv2.cvtColor(self.image_segmented, cv2.COLOR_BGR2GRAY)
-              _, binary_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY)
- 
-              #debut du teste apres avoir fixé step et threshold
-              for y in range(self.y, 100, -step):
-                if self.lines_above_surfer == 4:
-                  break
- 
-                sub_rect = binary_image[int(y - step/2):int(y + step/2), 0:binary_image.shape[1]]
-                line_percentage = np.count_nonzero(sub_rect) / (sub_rect.shape[0] * sub_rect.shape[1])
-                threshold = -5/1000*stepX+0.25
-                if line_percentage >threshold:
-                  if abs(y - last_y_center) > 100:
-                    self.lines_above_surfer +=1
-                    if self.lines_above_surfer ==1:
-                      self.hauteur_vague=self.y
-                      cv2.line(output_img, (0, y), (self.image_box.shape[1], y), (0, 255, 0), 5)
-                      cv2.putText(output_img, str(y), (10, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-                    last_y_center = y
- 
-              if self.lines_above_surfer <= 2 and self.lines_above_surfer >=1 :
-                break
-            if self.lines_above_surfer <= 2 and self.lines_above_surfer >=1 :
-              break
-          if self.lines_above_surfer <= 2 and self.lines_above_surfer >=1:
-            break
-        print(last_y_center)
-        print(self.lines_above_surfer)
-        self.image_with_lines = output_img
-        
 
     def calculate_line_percentage(self, sub_rect):
         return np.count_nonzero(sub_rect) / (sub_rect.shape[0] * sub_rect.shape[1])
@@ -154,16 +116,21 @@ class image_vague:
 
         for y in range(self.y, 100, -step):
             if lines_above_surfer == 4:
+            
                 break
                 
             
             # Update the range for x based on surfer's coordinates
-            x_start = max(0, int(self.x - 50))  # 50 pixels to the left of the surfer's center
-            x_end = min(binary_image.shape[1], int(self.x + 50))  # 50 pixels to the right of the surfer's center
+            quarter_width = self.image_original.shape[1] // 4
+            x_start = max(0, self.x - quarter_width // 2)
+            x_end = min(binary_image.shape[1], self.x + quarter_width // 2)
+            x_start = max(x_start, 0)
+            x_end = min(x_end, binary_image.shape[1])
+
 
             # Modify the line to use x_start and x_end
             #sub_rect = binary_image[int(y - step / 2):int(y + step / 2), x_start:x_end]
-            sub_rect =binary_image[int(y - step / 2):int(y + step / 2), 0:binary_image.shape[1]]
+            sub_rect = binary_image[int(y - step / 2):int(y + step / 2), 0:binary_image.shape[1]]
             line_percentage = self.calculate_line_percentage(sub_rect)
             threshold = -5 / 1000 * stepX + 0.25
 
@@ -172,16 +139,18 @@ class image_vague:
                     output_img, last_y_center, lines_above_surfer = self.process_detected_line(
                         output_img, lines_above_surfer, last_y_center, y
                     )
-
+        cv2.imshow("Lignes Detectees", output_img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         return output_img, last_y_center, lines_above_surfer
 
 
-    def dessiner_lignes_vertes_3(self, step=10, threshold_min=0, threshold_max=100):
+    def detection_haut_de_vague(self, step=10, threshold_min=0, threshold_max=100):
         for stepX in range(step, 40, 5):
             for threshold_high in range(threshold_min + 10, threshold_max, 10):
                 for threshold_low in range(threshold_min, threshold_high, 10):
                     # reinitialisation des variables
-                    self.lines_above_surfer = 0
+                    lines_above_surfer = 0
                     self.hauteur_vague = None
                     last_y_center = -1
                     self.detecter_contours(threshold_low, threshold_high)
@@ -203,30 +172,6 @@ class image_vague:
         print(last_y_center)
         print(lines_above_surfer)
         self.image_with_lines = output_img  
-        
-        
-    
-    
- 
-'''   
-#%%
-
-Vague=[]
-i=0
-for i in range(3):
-    Vague.append(image_vague("vague"+str(i)+".jpg"))
-    Vague[i].Taille_surfeur()
-    i=i+1
-
-
-#%%
-net = cv2.dnn.readNetFromDarknet('yolov4.cfg', 'yolov4.weights')
-
-
-surf=image_vague("vague1.jpg")
-surf.Taille_surfeur(net)
-#surf.affichage_image("vague1",surf.image_box)
-'''
 
 
 
